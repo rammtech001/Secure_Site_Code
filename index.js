18685facb5d21599594262decb732c3fff55cc31
rammtech001 <137055445+rammtech001@users.noreply.github.com>
2024-08-20 12:07:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function matfun() {
     location.href = "/page/Materials.html";
 }
+function monfun() {
+    location.href = "/page/mentoring.html";
+}
 
 function updatefun() {
     location.href = "/page/Updates.html";
